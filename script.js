8e9a3c0028bc828b66453d95c2ccb187b4405e61
geethajayashree2006-art <geethajayashree2006@gmail.com>
2025-09-10 11:27:39 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+// Smooth Scroll Effect for Navigation Links
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener("click", function(e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute("href")).scrollIntoView({
+            behavior: "smooth"
+        });
+    });
+});
+
+// Small greeting in console
+console.log("Welcome to Jayashree V's Portfolio!");
